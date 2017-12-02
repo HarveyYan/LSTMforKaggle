aefc6d6ae5a6b925b555de3f21e3c71cb90a1e47
Zichao Yan <yanzichao.cn@163.com>
2017-12-01 20:31:28 -0500

    utility addup

--- main.py
+++ main.py
@@ -8,39 +8,50 @@ from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.preprocessing.sequence import pad_sequences
 import pandas as pd
+from keras.utils import plot_model
+import matplotlib.pyplot as plt
+import os
 
-#####################
-# Hyperparameters
-#####################
+'''
+hyper parameters
+'''
 MAX_NB_WORDS = 20000
 MAX_SEQUENCE_LENGTH = 30
 BATCH_SIZE = 10
 EMBEDDING_DIM = 300
 VALIDATION_SPLIT = 0.1
 
+'''
+optimizable hyperparams
+'''
 num_lstm = np.random.randint(175, 275)
 num_dense = np.random.randint(100, 150)
 rate_drop_lstm = 0.15 + np.random.rand() * 0.25
 rate_drop_dense = 0.15 + np.random.rand() * 0.25
 act = 'relu'
 re_weight = True
-#####################
-# global variable for optimization
-#####################
+glove = False
+
 embedding_mat = None
 length = None
 STAMP = 'lstm_%d_%d_%.2f_%.2f'%(num_lstm, num_dense, rate_drop_lstm, rate_drop_dense)
-#####################
-# load pre-trained word embeddings
-#####################
+outpath = './result/' + STAMP + '/'
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+
+'''
+load pretrained word embeddings, whether it's word2vec or glove
+'''
 def load_word2vec():
     model = gensim.models.KeyedVectors.load_word2vec_format('word_embedding/GoogleNews-vectors-negative300.bin.gz', binary=True)
     return model
-#####################
-# embeddings for embedding layer of the lstm
-#####################
+
+'''
+embedding matrix for embedding layer
+'''
 def generate_embeddings(model, tokenizer):
     print('Generating word embeddings')
+    missing_vocab = open(outpath +'missing_vocab.txt','r')
     global embedding_mat, length
     word_index = tokenizer.word_index
     print('total words found:', len(word_index))
@@ -53,9 +64,34 @@ def generate_embeddings(model, tokenizer):
             embedding_mat[i] = model.word_vec(word)
         else:
             print(word, 'not in model vocab')
-#####################
-# training batch data
-#####################
+            missing_vocab.write(word+'\n')
+    missing_vocab.close()
+
+
+
+def plot_some_curves(history):
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.savefig(outpath +'model_acc.png')
+    plt.close()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.savefig(outpath + 'model_loss.png')
+    plt.close()
+
+
+'''
+obsolete
+'''
 def get_a_batch(generator, tokenizer):
     batch = []
     for i in range(BATCH_SIZE):
@@ -104,21 +140,15 @@ class LSTM_model:
         normalized_dense = BatchNormalization()(Dropout(rate_drop_dense)(dense))
 
         preds = Dense(1, activation='sigmoid')(normalized_dense)
-        #####################
-        # assemble the model
-        #####################
+
         self.net = Model(inputs=[sequence_1_input, sequence_2_input], outputs= preds)
         self.net.compile(
             loss='binary_crossentropy',
             optimizer='nadam',
             metrics=['acc']
         )
-        print(STAMP)
 
     def train(self,train_1, train_2, labels):
-        #####################
-        # gather the data
-        #####################
         indices = np.random.permutation(len(train_1))
         idx_train = indices[:int(len(train_1)*(1-VALIDATION_SPLIT))]
         idx_val = indices[int(len(train_1) * (1 - VALIDATION_SPLIT)):]
@@ -137,28 +167,29 @@ class LSTM_model:
             weight_val *= 0.472001959
             weight_val[labels_val == 0] = 1.309028344
 
-        #####################
-        # train the model
-        #####################
-
         early_stopping = EarlyStopping(monitor='val_loss', patience=3)
-        best_model_path = STAMP + '.h5'
+        best_model_path = outpath + 'weight.h5'
         model_checkpoint = ModelCheckpoint(best_model_path, save_best_only=True, save_weights_only=True)
         hist = self.net.fit([data_1_train, data_2_train], labels_train,
                      validation_data=([data_1_val,data_2_val],labels_val,weight_val),
                      epochs=200, batch_size=2048,shuffle=True,
                      class_weight=self.class_weights, callbacks=[early_stopping, model_checkpoint])
+
+        plot_model(self.net, to_file=outpath+'net.png')
+        np.savetxt(outpath + 'acc.txt', np.array(hist.history['acc']), delimiter=',')
+        np.savetxt(outpath + 'val_acc.txt', np.array(hist.history['val_acc']), delimiter=',')
+        np.savetxt(outpath + 'loss.txt', np.array(hist.history['loss']), delimiter=',')
+        np.savetxt(outpath + 'val_loss.txt', np.array(hist.history['val_loss']), delimiter=',')
+        plot_some_curves(hist)
+
         self.net.load_weights(best_model_path)
-        best_val_score = min(hist.history['val_loss'])
-        print('best validation score', best_val_score)
-        return best_val_score
 
     def predict(self, test_1, test_2, ids, best_val_score):
-        preds = model.predict([test_1, test_2], batch_size=8192, verbose=1)
-        preds += model.predict([test_2, test_1], batch_size=8192, verbose=1)
+        preds = self.net.predict([test_1, test_2], batch_size=8192, verbose=1)
+        preds +=self.net.predict([test_2, test_1], batch_size=8192, verbose=1)
         preds /= 2
         submission = pd.DataFrame({'test_id': ids, 'is_duplicate': preds.ravel()})
-        submission.to_csv('%.4f_' % (best_val_score) + STAMP + '.csv', index=False)
+        submission.to_csv(outpath + 'prediction.csv', index=False)
 
 '''
 todo, process training and testing data
